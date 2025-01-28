144a36f90f1de5d5ea1756150f72eee11d21d203
M Qasim Irfan <qasimirfan02@gmail.com>
2025-01-28 11:33:25 +0500

    first commit

---
+++ deepface/api/src/app.py
@@ -0,0 +1,19 @@
+# 3rd parth dependencies
+from flask import Flask
+from flask_cors import CORS
+
+# project dependencies
+from deepface import DeepFace
+from deepface.api.src.modules.core.routes import blueprint
+from deepface.commons.logger import Logger
+
+logger = Logger()
+
+
+def create_app():
+    app = Flask(__name__)
+    # CORS(app)
+    CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
+    app.register_blueprint(blueprint)
+    logger.info(f"Welcome to DeepFace API v{DeepFace.__version__}!")
+    return app
