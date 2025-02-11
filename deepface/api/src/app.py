4785165878d4d5fa5cfdeb0bbcb00871a29b70c2
Qasim Irfan <qasimirfan02@gmail.com>
2025-02-11 21:34:27 +0500

    3rd commit

--- deepface/api/src/app.py
+++ deepface/api/src/app.py
@@ -1,18 +1,46 @@
-# 3rd parth dependencies
+# # 3rd parth dependencies
+# from flask import Flask
+# from flask_cors import CORS
+
+# # project dependencies
+# from deepface import DeepFace
+# from deepface.api.src.modules.core.routes import blueprint
+# from deepface.commons.logger import Logger
+
+# logger = Logger()
+
+
+# def create_app():
+#     app = Flask(__name__)
+#     CORS(app)  
+#     app.register_blueprint(blueprint)
+#     logger.info(f"Welcome to DeepFace API v{DeepFace.__version__}!")
+#     return app
+
 from flask import Flask
 from flask_cors import CORS
 
-# project dependencies
+# Import DeepFace-related dependencies
 from deepface import DeepFace
 from deepface.api.src.modules.core.routes import blueprint
 from deepface.commons.logger import Logger
 
 logger = Logger()
 
-
 def create_app():
     app = Flask(__name__)
-    CORS(app)
+    
+    # Allow specific origins instead of '*'
+    CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+
+    @app.after_request
+    def add_cors_headers(response):
+        response.headers["Access-Control-Allow-Origin"] = "*"
+        response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+        response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+        return response
+
     app.register_blueprint(blueprint)
     logger.info(f"Welcome to DeepFace API v{DeepFace.__version__}!")
     return app
+
